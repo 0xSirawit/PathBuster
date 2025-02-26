bc2ea0125e4db25729612a9e5d4e7ad5673fa208
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-26 23:13:28 +0700

    Refactor asyncio handling use gather with batching to improve performance

--- pathbuster.py
+++ pathbuster.py
@@ -4,7 +4,6 @@ import asyncio
 import time
 
 FILTER_STATUS_CODE = {404}
-RESULT = []
 
 
 class bcolors:
@@ -19,7 +18,7 @@ class bcolors:
     UNDERLINE = "\033[4m"
 
 
-async def fetch(session, guess_url):
+async def fetch(session, guess_url: str) -> None:
     global error
     try:
         async with session.get(guess_url) as response:
@@ -34,20 +33,19 @@ async def fetch(session, guess_url):
                     )
                 else:
                     print(f"{response.status} {guess_url} {response.content_length}")
-                RESULT.append((response.status, guess_url, response.content_length))
     except:
         error += 1
 
 
-async def brute(url, wordlist):
+async def brute(url: str, wordlist: list, batch_size: int = 100) -> None:
     async with aiohttp.ClientSession() as session:
-        for word in wordlist:
-            guess_url = f"{url}/{word}"
-            task = asyncio.create_task(fetch(session, guess_url))
-            await task
+        for i in range(0, len(wordlist), batch_size):
+            batch = wordlist[i : i + batch_size]
+            tasks = [fetch(session, f"{url}/{word}") for word in batch if word]
+            await asyncio.gather(*tasks)
 
 
-def read_wordlist(filename):
+def read_wordlist(filename: str) -> list:
     try:
         with open(filename, "r", encoding="utf-8") as f:
             wordlist = f.read().splitlines()
@@ -61,13 +59,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="Target URL for brute-force path discovery")
     parser.add_argument("--wordlist", help="wordlist")
+    parser.add_argument(
+        "--batch-size", help="Batch size for concurrent requests", type=int
+    )
     args = parser.parse_args()
     url = args.url.rstrip("/")
     error = 0
 
     try:
         start_time = time.time()
-        asyncio.run(brute(url, read_wordlist(args.wordlist)))
+        asyncio.run(brute(url, read_wordlist(args.wordlist), args.batch_size))
     except KeyboardInterrupt:
         pass
     finally:

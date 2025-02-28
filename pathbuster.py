12abcac4bc4289d47f62bbd7d91d975363d9228c
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-28 18:48:16 +0700

    Add live status  (req/sec)

--- pathbuster.py
+++ pathbuster.py
@@ -2,42 +2,38 @@ import argparse
 import aiohttp
 import asyncio
 import time
+import sys
+from threading import Thread
 
 FILTER_STATUS_CODE = {404}
 
 
 class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
     OKGREEN = "\033[92m"
     WARNING = "\033[93m"
-    FAIL = "\033[91m"
     ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
 
 
 async def fetch(session, guess_url: str) -> None:
-    global error
+    global error, req
     try:
         async with session.get(guess_url) as response:
             if response.status not in FILTER_STATUS_CODE:
-                if response.status == 200:
-                    print(
-                        f"{bcolors.OKGREEN}{response.status}{bcolors.ENDC} {guess_url} {response.content_length}"
-                    )
-                elif response.status == 403:
-                    print(
-                        f"{bcolors.WARNING}{response.status}{bcolors.ENDC} {guess_url} {response.content_length}"
-                    )
-                else:
-                    print(f"{response.status} {guess_url} {response.content_length}")
+                status_color = (
+                    bcolors.OKGREEN if response.status == 200 else bcolors.WARNING
+                )
+                print(
+                    f"{status_color}{response.status}{bcolors.ENDC} {guess_url} {response.content_length}"
+                )
     except:
         error += 1
+    finally:
+        req += 1
 
 
-async def brute(url: str, wordlist: list, batch_size: int = 100) -> None:
+async def brute(url: str, wordlist: list, batch_size: int) -> None:
+    if not batch_size:
+        batch_size = 100
     async with aiohttp.ClientSession() as session:
         for i in range(0, len(wordlist), batch_size):
             batch = wordlist[i : i + batch_size]
@@ -55,6 +51,15 @@ def read_wordlist(filename: str) -> list:
     return wordlist
 
 
+def monitor():
+    global req, running
+    while running:
+        time.sleep(1)
+        sys.stdout.write(f"{req} reqs/sec\r")
+        sys.stdout.flush()
+        req = 0
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="Target URL for brute-force path discovery")
@@ -65,13 +70,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     url = args.url.rstrip("/")
     error = 0
+    req = 0
+    running = True
 
     try:
         start_time = time.time()
+        monitor_thread = Thread(target=monitor, daemon=True)
+        monitor_thread.start()
         asyncio.run(brute(url, read_wordlist(args.wordlist), args.batch_size))
     except KeyboardInterrupt:
-        pass
+        running = False
     finally:
         end_time = time.time()
+        monitor_thread.join()
         print(f"Total errors: {error}")
         print(f"Execution time: {end_time - start_time:.2f} seconds")
